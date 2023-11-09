66796a107f3ea4cab697f56733a3eaadb454cf14
vladajankovic <vladajankovic1999@gmail.com>
2023-11-10 00:27:24 +0100

    Update Main.java

--- Internship task/src/Main.java
+++ Internship task/src/Main.java
@@ -7,14 +7,13 @@ import utility.Utility;
 
 public class Main {
 
-
-public static void main(String[] args) {
-        new Utility();
+	public static void main(String[] args) {
+		new Utility();
         // Create cards for player 1's deck
         List<Card> player1Deck = Utility.generateCards();
    
         // Create cards for player 2's deck
-       List<Card> player2Deck = Utility.generateCards();
+        List<Card> player2Deck = Utility.generateCards();
 
         Player player1 = new Player(20,player1Deck);
         Player player2 = new Player(20,player2Deck);
