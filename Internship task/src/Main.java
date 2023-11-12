42a733bac8deaeb693a980463da0aa4d48889408
vladajankovic <vladajankovic1999@gmail.com>
2023-11-12 01:09:47 +0100

    Update Main.java

--- Internship task/src/Main.java
+++ Internship task/src/Main.java
@@ -4,23 +4,22 @@ import game.Game;
 import player.Player;
 import utility.Utility;
 
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-        // Create cards for player 1's deck
-        List<Card> player1Deck = Utility.generateCards();
-   
-        // Create cards for player 2's deck
-        List<Card> player2Deck = Utility.generateCards();
 
-        Player player1 = new Player(20,player1Deck);
-        Player player2 = new Player(20,player2Deck);
-        
-        // Create a game instance
-        Game game = new Game(player1, player2);
-        game.startGame();
+		// Create cards for player 1's deck
+		List<Card> player1Deck = Utility.generateCards();
 
-    }
+		// Create cards for player 2's deck
+		List<Card> player2Deck = Utility.generateCards();
+
+		Player player1 = new Player(20, player1Deck);
+		Player player2 = new Player(20, player2Deck);
+
+		// Create a game instance
+		Game game = new Game(player1, player2);
+		game.startGame();
+
+	}
 }
