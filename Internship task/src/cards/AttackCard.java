e57642da94037c0361c0ca826fd0e8f840016387
vladajankovic <vladajankovic1999@gmail.com>
2023-11-12 01:08:55 +0100

    Update AttackCard.java

--- Internship task/src/cards/AttackCard.java
+++ Internship task/src/cards/AttackCard.java
@@ -1,24 +1,24 @@
 package cards;
 
 public class AttackCard implements Card {
-    private int number;
+	private int number;
 
-    public AttackCard(int number) {
-        this.number = number;
-    }
+	public AttackCard(int number) {
+		this.number = number;
+	}
 
-    @Override
-    public int getNumber() {
-        return number;
-    }
+	@Override
+	public int getNumber() {
+		return number;
+	}
 
-    @Override
-    public void effect() {
-        System.out.println("Attack card effect activated!\r\n");
-    }
+	@Override
+	public void effect() {
+		System.out.println("Attack card effect activated!\r\n");
+	}
 
-    @Override
-    public String description(){
-        return "Attack card";
-    }
+	@Override
+	public String description() {
+		return "Attack card";
+	}
 }
