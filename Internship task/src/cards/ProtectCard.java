3432f6613cf8b9b6adde439aa18d1e279b71ef78
vladajankovic <vladajankovic1999@gmail.com>
2023-11-12 01:08:21 +0100

    Update ProtectCard.java

--- Internship task/src/cards/ProtectCard.java
+++ Internship task/src/cards/ProtectCard.java
@@ -2,25 +2,25 @@ package cards;
 
 public class ProtectCard implements Card {
 
-    private int number;
+	private int number;
 
-    public ProtectCard(){
-        this.number = 1;
-    }
+	public ProtectCard() {
+		this.number = 1;
+	}
 
-    @Override
-    public int getNumber() {
-      return number;
-    }
+	@Override
+	public int getNumber() {
+		return number;
+	}
 
-    @Override
-    public void effect() {
-        System.out.println("Protect card effect activated!\r\n");
-    }
+	@Override
+	public void effect() {
+		System.out.println("Protect card effect activated!\r\n");
+	}
 
-    @Override
-    public String description(){
-        return "Protect card";
-    }
-    
+	@Override
+	public String description() {
+		return "Protect card";
+	}
+
 }
