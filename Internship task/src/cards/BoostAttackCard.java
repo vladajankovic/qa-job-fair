c05d1ace251b946116cc67f02e5aafb6ab831aca
vladajankovic <vladajankovic1999@gmail.com>
2023-11-12 01:08:43 +0100

    Update BoostAttackCard.java

--- Internship task/src/cards/BoostAttackCard.java
+++ Internship task/src/cards/BoostAttackCard.java
@@ -1,30 +1,30 @@
 package cards;
 
-public class BoostAttackCard implements Card{
-    private int number;
-    private static int boost = 3;
+public class BoostAttackCard implements Card {
+	private int number;
+	private static int boost = 3;
 
-    public BoostAttackCard(){
-        this.number = 2;
-    }
+	public BoostAttackCard() {
+		this.number = 2;
+	}
 
-    @Override
-    public int getNumber() {
-        return number;
-    }
+	@Override
+	public int getNumber() {
+		return number;
+	}
 
-    public int getBoost(){
-        return boost;
-    }
+	public int getBoost() {
+		return boost;
+	}
 
-    @Override
-    public void effect() {
-        System.out.println("Boost Attack card effect activated!\n"
-        		+ "Player have increased attack damage in this turn by " + boost + "!\r\n");
-    }
+	@Override
+	public void effect() {
+		System.out.println("Boost Attack card effect activated!\n"
+				+ "Player have increased attack damage in this turn by " + boost + "!\r\n");
+	}
 
-    @Override
-    public String description(){
-        return "Boost card";
-    }
+	@Override
+	public String description() {
+		return "Boost card";
+	}
 }
